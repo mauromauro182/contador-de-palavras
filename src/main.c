7d48c1d3eeaf8a6d2e32039d463de2f892ebcc57
mauromauro182 <30840779+mauromauro182@users.noreply.github.com>
2017-11-13 22:50:48 -0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,10 +13,26 @@ int main() {
 
   c = 1;
 
+  int esp=1;
+  int flagE=0;
+  int total=0;
+
   while (c != '\n') {
     scanf("%c", &c);
+	if(c== ' '||c== '.' || c== ','|| c== '-'){
+		if(flagE==0){
+			esp=esp+1;
+			flagE=1;
+			}		
+	}else flagE=0;
+
+	if(c== '0'||c== '1'||c== '2'||c== '3'||c== '4'||c== '5'||c== '6'||c== '7'||c== '8'||c== '9'){
+		flagE=1;		
+	}
   }
 
-  printf("1\n");
+	total= esp;
+
+  printf("%d\n", total);
   return 0;
 }
